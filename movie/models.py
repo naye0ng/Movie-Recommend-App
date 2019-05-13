da52fd55f14db4625d3a2659fd1f75d83967b060
Hee-Don <don210198@gmail.com>
2019-05-13 14:32:11 +0900

    movie 어플리케이션 Model 정의
    - urlpatterns 추가
    - Movie, Genre, Review 모델 추가

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    poster_url = models.CharField(max_length=200)
+    trailer_url = models.CharField(max_length=200)
+    audience = models.IntegerField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    score = models.IntegerField()
+    comment = models.CharField(max_length=300)
