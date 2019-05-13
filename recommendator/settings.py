58818ab05e39bcce81a5fe18a4a759c2fbb84571
Hee-Don <don210198@gmail.com>
2019-05-13 10:56:38 +0900

    어플리케이션 movie 추가

--- recommendator/settings.py
+++ recommendator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wgntr#4sx*-m%@m6@i%%32x#3iz@+^!sp-4=)h$tcfsw6zx*hu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movie',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
