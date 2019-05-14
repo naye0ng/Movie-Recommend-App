4eed154003cb6dbe15a4c3bb47c7f9508e123bcb
Hee Don Yeom <don210198@gmail.com>
2019-05-14 04:19:07 +0000

    rest framework swagger가 작동하지 않던 issue 해결

--- recommendator/settings.py
+++ recommendator/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
