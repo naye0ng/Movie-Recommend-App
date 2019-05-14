3c7bd98e1f8ef18f0e305a2625f780f1a966ed38
Hee Don Yeom <don210198@gmail.com>
2019-05-14 04:19:45 +0000

    url 변경: comment에서 review로 변경

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('movies/', views.movies), # GET, POST
     path('movies/<int:movie_id>/',views.movie_detail), # GET, PUT, DELETE
     path('movies/<int:movie_id>/like/',views.movie_like), # GET
-    path('movies/<int:movie_id>/comments/',views.comments), # GET, POST
-    path('movies/<int:movie_id>/comments/<int:comment_id>/',views.comment_detail), # PUT, DELETE
+    path('movies/<int:movie_id>/reviews/',views.reviews), # GET, POST
+    path('movies/<int:movie_id>/reviews/<int:review_id>/',views.review_detail), # PUT, DELETE
     path('users/', views.users), # GET
     path('users/<int:user_id>/', views.user_detail), # GET, PUT, DELETE
     path('users/<int:user_id>/follow/', views.user_follow), # GET
