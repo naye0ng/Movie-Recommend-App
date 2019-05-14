41107e1dec549ff822d1dd6e2962d7decf9d7fff
Hee Don Yeom <don210198@gmail.com>
2019-05-14 01:40:54 +0000

    moive 기능 구현
    - movies: 전체 영화 조회 기능
    - movie_detail: 세부 영화 조회, 게시, 수정 기능
    - movie_like: 영화 좋아요 기능

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,22 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from movie.models import Genre, Movie, Review
+from movie.serializers import MovieSerializer, ReviewSerailizer
+from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
 
 # don2101
+@api_view(['GET', 'POST'])
 def movies(request):
+    movie_list = Movie.objects.all()
+    serializer = Response(movie_list, many=True)
+    
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    
+    if request.method == 'GET':
+        serializer = MovieSerializer(movie)
+        
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response({'message': '작성 완료'})
+        return Response(serializer.error)
+    else:
+        serializer = MovieSerializer(movie, data=request.data, partial=True)
+        
+        if serial.is_valid():
+            serializer.save()
+            
+            return Response({'message': '수정 완료'})
+        return Response(serializer.error)
+
+
+@login_required
+@api_view(['GET'])
+def movie_like(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    
+    if request.user in movie.user_like.all():
+        movie.user_like.remove(request.user)
+        
+        return Response({'message': 'unlike'})
+    else:
+        movie.user_like.add(request.user)
+        
+        return Response({'message': 'like'})
+    
+    
+def comments(request):
     pass
 
-def movie_detail(request) :
-    pass
-
-def movie_like(request) :
-    pass
-
-def comments(request) :
-    pass
-
-def comment_detail(request) :
+def comment_detail(request):
     pass
 
 
@@ -26,7 +71,7 @@ def users(request) :
 
 def user_detail(request) :
     pass
-
+`
 def user_follow(request) :
     pass
 
