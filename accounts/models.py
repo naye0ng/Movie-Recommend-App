3d2f4aeadf6e156425d1f1ea4e64f108831ef054
Hee Don Yeom <don210198@gmail.com>
2019-05-15 21:04:46 +0000

    User 모델에 like_genre column 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from movie.models import Genre
+
 
 # Create your models here.
 class User(AbstractUser) :
     isAdmin = models.BooleanField(default=False)
     # followers : 자신이 follow 하는 User
     # followings : 자신을 follow 하고 있는 User
-    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followings',blank=True)
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followings', blank=True)
+    # like_genre : user가 좋아하는 genre
+    # liked_by : genre를 좋아하는 user
+    like_genre = models.ManyToManyField(Genre, related_name='liked_by', blank=True)
